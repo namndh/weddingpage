38fe10c8442268fba8630ca9ccfdce74f51e2d98
namndh <hainam.nguyen.ict@gmail.com>
2024-03-22 01:19:24 +0700

    Register RSVP model

--- singlepage/admin.py
+++ singlepage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Wish, RSVP
+
 # Register your models here.
+
+
+admin.site.register(Wish)
+admin.site.register(RSVP)
--- singlepage/models.py
+++ singlepage/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+
 # Create your models here.
 
 
@@ -9,3 +11,22 @@ class Wish(models.Model):
 
     def __str__(self):
         return f"{self.wish}-{self.author}"
+
+
+class RSVP(models.Model):
+    ATTENDING_OPTIONS = (
+        ("Có", "Có"),
+        ("Không", "Không")
+    )
+    PARTY_OPTIONS = (
+        ("Nhà Trai", "Nhà Trai"),
+        ("Nhà Gái", "Nhà Gái")
+    )
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    attendance = models.CharField(max_length=10, choices=ATTENDING_OPTIONS, default="Có")
+    party = models.CharField(max_length=10, choices=PARTY_OPTIONS, default="Nhà Trai")
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}-{self.email}"
