0b32779fbda3bc355c307014a7dacd44c4b1157b
namndh <hainam.nguyen.ict@gmail.com>
2024-03-23 02:46:12 +0700

    add cors settings

--- weddingsite/settings.py
+++ weddingsite/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://pure-wave-49292-087980e733a6.herokuapp.com"]
 
-CSRF_TRUSTED_ORIGINS = [" https://pure-wave-49292-087980e733a6.herokuapp.com"]
+CSRF_TRUSTED_ORIGINS = ["https://pure-wave-49292-087980e733a6.herokuapp.com"]
 
 # Application definition
 
